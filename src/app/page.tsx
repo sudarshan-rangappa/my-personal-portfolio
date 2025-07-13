b50dbeba6620f2c2ddf57e66787bd30c12853093
sudarshan-rangappa <sudarshan_rangappa@proton.me>
2025-07-14 02:25:48 +0530

    Added Timeline section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/Home/HeroSection";
+import { TimelineDemo } from "./components/Home/Timeline";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <TimelineDemo />
     </div>
   );
 }
