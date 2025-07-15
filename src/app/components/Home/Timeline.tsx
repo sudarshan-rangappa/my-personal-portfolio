f150bd4620ded81f88b1115c845fa443083ed41a
sudarshan-rangappa <sudarshan_rangappa@proton.me>
2025-07-15 22:43:38 +0530

    final finished timeline till bcu

--- src/app/components/Home/Timeline.tsx
+++ src/app/components/Home/Timeline.tsx
@@ -86,32 +86,32 @@ export function TimelineDemo() {
 
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://i.postimg.cc/Dy26xQ6x/NFSU1.jpg"
+              src="https://i.postimg.cc/3Nffm96f/NFSU1.jpg"
               alt="nfsu podium"
               width={500}
               height={500}
-              className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover object-top shadow-lg"
+              className="h-20 w-full rounded-lg object-cover object-top shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
               src="https://i.postimg.cc/DzXjsvfs/NFSU00.jpg"
               alt="feature template"
               width={500}
               height={500}
-              className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-center shadow-lg"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
               src="https://i.postimg.cc/4dkqy7gj/NFSU3.jpg"
               alt="classroom friends"
               width={500}
               height={500}
-              className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover shadow-lg"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
             <img
               src="https://i.postimg.cc/Rh8cCfrH/nfsu4.jpg"
               alt="ethnic day"
               width={500}
               height={500}
-              className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover shadow-lg"
+              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
             />
           </div>
         </div>
