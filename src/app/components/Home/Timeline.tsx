39621c4fd95cb106a6e201c4b6a23c893d1c4806
sudarshan-rangappa <sudarshan_rangappa@proton.me>
2025-07-15 22:04:42 +0530

    Completed NFSU section in timeline

--- src/app/components/Home/Timeline.tsx
+++ src/app/components/Home/Timeline.tsx
@@ -56,7 +56,7 @@ export function TimelineDemo() {
           </h3>
 
           <h4 className="mb-2 text-xl md:text-2xl font-bold text-[#00e0ff]">
-            CGPT: 8.24
+            CGPA: 8.24
           </h4>
 
           <p className="mb-2 text-sm md:text-base font-normal text-neutral-400">
@@ -85,35 +85,36 @@ export function TimelineDemo() {
           </p>
 
           <div className="grid grid-cols-2 gap-4">
-            <img
-              src="https://assets.aceternity.com/pro/hero-sections.png"
-              alt="hero template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/features-section.png"
-              alt="feature template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/pro/bento-grids.png"
-              alt="bento template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-            <img
-              src="https://assets.aceternity.com/cards.png"
-              alt="cards template"
-              width={500}
-              height={500}
-              className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-            />
-          </div>
+  <img
+    src="https://i.postimg.cc/Dy26xQ6x/NFSU1.jpg"
+    alt="nfsu podium"
+    width={500}
+    height={500}
+    className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover object-top shadow-lg"
+  />
+  <img
+    src="https://i.postimg.cc/DzXjsvfs/NFSU00.jpg"
+    alt="feature template"
+    width={500}
+    height={500}
+    className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-center shadow-lg"
+  />
+  <img
+    src="https://i.postimg.cc/4dkqy7gj/NFSU3.jpg"
+    alt="classroom friends"
+    width={500}
+    height={500}
+    className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover shadow-lg"
+  />
+  <img
+    src="https://i.postimg.cc/Rh8cCfrH/nfsu4.jpg"
+    alt="ethnic day"
+    width={500}
+    height={500}
+    className="h-40 md:h-64 lg:h-[320px] w-full rounded-lg object-cover shadow-lg"
+  />
+</div>
+
         </div>
       ),
     },
