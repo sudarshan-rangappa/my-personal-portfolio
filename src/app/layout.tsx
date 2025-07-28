30959fcf3f5290ec968f99d9f68b9800f659cf22
RizWaaN3024 <rizwaanans786@gmail.com>
2025-07-29 02:31:15 +0530

    OpenGraph configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,28 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
     title: "sudo ./portfolio",
     description: "Explore Sudarshan Rangappa’s live portfolio showcasing responsive web design, Red & Blue Team demos, and penetration testing tools. —open to feedback and collaboration.",
+    openGraph: {
+        title: 'root@sudarshan',
+        description: 'Explore Sudarshan Rangappa’s cybersecurity lab with Red Team attacks, Blue Team defense, SIEM dashboards, SOAR workflows, and more. Built for cyber professionals and enthusiasts.',
+        url: 'https://www.sudarshanrangappa.in.net/',
+        siteName: 'root@sudarshan',
+        images: [
+            {
+                url: '/opengraphss.png',
+                width: 1200,
+                height: 630,
+                alt: 'root@sudarshan – Cybersecurity Portfolio Preview',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title: 'root@sudarshan',
+        description: 'Sudarshan Rangappa’s hacker-style portfolio featuring threat emulation, detection engineering, and Blue/Red Team labs.',
+        images: ['/opengraphss.png'],
+    }
 };
 
 export default function RootLayout({
